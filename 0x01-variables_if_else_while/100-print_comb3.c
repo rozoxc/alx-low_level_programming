263aaa24b48cbf7ce3b368e1dd5b63251c2cdeda
a@fma.com <you@email.com>
2023-09-08 16:34:11 -0700

    ls

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 0;
+	c = 0;
+	while (a <= 8)
+	{
+		b = c + 1;
+		while (b <= 9)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+		if (a != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		c++;
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
