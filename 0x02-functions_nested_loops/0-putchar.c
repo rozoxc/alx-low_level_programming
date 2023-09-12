5749b874971911508b0350dd6aa9ed89d56ceb67
a@fma.com <you@email.com>
2023-09-12 04:59:40 -0700

    ls

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
