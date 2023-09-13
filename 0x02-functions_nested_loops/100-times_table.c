9441e8fd62431566a73cf95245875f3433231169
a@fma.com <you@email.com>
2023-09-13 07:39:17 -0700

    ld

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,28 @@
 #include "main.h"
-#include <stdlib.h>
+/**
+ * This function takes an integer 'n' as input and prints the times table
+ * for that number from 1 to 10.
+ *
+ * @param n The number for which to print the times table.
+ */
 
+void _print_space(void)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+}
+void _print_number(int n)
+{
+	_putchar(n / 10 + '0');
+	_putchar(n % 10 + '0');
+}
 void print_times_table(int n)
 {
 	int i, j, a, c;
 
 	i = 0;
-	if (n < 15 && n > 0)
+	if (n < 15 && n >= 0)
 	{
 		while (i <= n)
 		{
@@ -16,9 +32,7 @@ void print_times_table(int n)
 				a = i * j;
 				if (j != 0 && a < 10)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					_print_space();
 				}
 				if (a >= 10)
 				{
@@ -26,15 +40,13 @@ void print_times_table(int n)
 					if (a >= 100)
 					{
 						c = a / 10;
-						_putchar(c / 10 + '0');
-						_putchar(c % 10 + '0');
+						_print_number(c);
 						_putchar(a % 10 + '0');
 					}
 					else
 					{
 						_putchar(' ');
-						_putchar(a / 10 + '0');
-						_putchar(a % 10 + '0');
+						_print_number(a);
 					}
 				}
 				else
