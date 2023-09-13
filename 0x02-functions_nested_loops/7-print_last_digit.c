c32570cd799df624c3421ca698081ce8c1832133
a@fma.com <you@email.com>
2023-09-12 18:11:34 -0700

    ls

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- * @n: The number to extract the last digit from
- *
- * Return: The value of the last digit
+ * print_last_digit - prints the last digit of a number.
+ * @n: the number as argument.
+ * Return: tha last digit of the number.
  */
+
 int print_last_digit(int n)
 {
-	int last_digit;
-
+	if (n == -2147483648)
+	{
+		_putchar('8');
+		return (8);
+	}
 	if (n < 0)
-		n = -n;
-	last_digit = n % 10;
-	_putchar('0' + last_digit);
-	return (last_digit);
+		n *= -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
-
