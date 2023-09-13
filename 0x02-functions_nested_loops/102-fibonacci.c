892faa9b602c1f1692ac453b1b11a7162cc7d50a
a@fma.com <you@email.com>
2023-09-13 15:15:47 -0700

    ls

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long long int F[50];
+
+	F[0] = 0;
+	F[1] = 1;
+	i = 2;
+	while (i <= 50)
+	{
+		F[i] = F[i - 1] + F[i - 2];
+		i++;
+	}
+	i = 0;
+	while (i <= 49)
+	{
+		printf("%llu", F[i]);
+		if (i != 49)
+		{
+			printf(" ,");
+		}
+		printf("\n");
+		i++;
+	}
+	return (0);
+}
