c12c109dbf73be6a6e2c05a5a9874006bdddd66a
a@fma.com <you@email.com>
2023-09-14 06:12:37 -0700

    push

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void positive_or_negative(int n)
+{
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+}
