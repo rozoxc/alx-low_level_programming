ae5301cba31c2068610f3644e6a993bd136700d3
a@fma.com <you@email.com>
2023-09-14 15:29:45 -0700

    ld

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,14 @@
 #include "main.h"
 /**
- * more_numbers - print number for 1 to 14 10 times
+ * _new_line - print space
  */
-
+void _new_line(void)
+{
+	_putchar('\n');
+}
+/**
+ * more_numbers - print numbers from 0 to 14 10 times
+ */
 void more_numbers(void)
 {
 	int num, i;
@@ -15,13 +21,14 @@ void more_numbers(void)
 		{
 			if (num >= 10)
 			{
+				_putchar(num / 10 + '0');
 				_putchar(num % 10 + '0');
 			}
 			else
 				_putchar(num + '0');
 			num++;
 		}
-		_putchar('\n');
+		_new_line();
 		i++;
 	}
 }
