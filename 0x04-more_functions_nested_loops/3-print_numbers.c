7d4ee38e2d28d3588be57b4bba158b3d80d3fb0e
a@fma.com <you@email.com>
2023-09-14 14:36:12 -0700

    ls

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,17 @@
 #include "main.h"
+/**
+ * print_numbers - print number from 0 to 9
+ */
 
-void print_numbers(void);
+void print_numbers(void)
 {
 	char a;
 
 	a = '0';
-	while (a <= 'z')
+	while (a <= '9')
 	{
 		_putchar(a);
 		a++;
 	}
+	_putchar('\n');
 }
