fd8fde642d1a1d084c7947fe71e9e80e2dab015f
a@fma.com <you@email.com>
2023-09-19 01:21:51 -0700

    dl

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - function who print array
+ * @a: the array
+ * @n: the order of this array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n - 1)
+	{
+		printf("%d", a[i]);
+		printf(",");
+		printf(" ");
+		i++;
+	}
+	printf("\n");
+}
