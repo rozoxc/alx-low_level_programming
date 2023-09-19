7fd50d02eb777fc0be942c5145eee6e9a63301a7
a@fma.com <you@email.com>
2023-09-18 17:34:09 -0700

    ld

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function print half string
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
