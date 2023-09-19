58afd4e54dc16d9fc4b85c2eb8fe0cc420107aa5
a@fma.com <you@email.com>
2023-09-18 17:16:27 -0700

    dL

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - function return the size of strin
+ *  @s : stringi
+ *
+ *  Return: return integers
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * print_rev - reverse string
+ * @s : string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s) - 1;
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void _putchar(int c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 
 
 #endif
