677c43deac45c6ec157db4e9f92d481d2c20a9df
a@fma.com <you@email.com>
2023-09-19 07:53:59 -0700

    ld

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * _atoi -  It seem the atoi function
+ * @s: teh string
+ * Return: return the integers
+ */
+
+int _atoi(char *s)
+{
+	int i, a, c;
+
+	i = 0;
+	a = 1;
+	c = 0;
+	if (s[i] != '\0')
+	{
+		while (s[i] >=  58 || s[i] <= 47)
+		{
+			if (s[i] == 45)
+				a *= -1;
+			i++;
+		}
+		while (s[i] >= 48 && s[i]  <= 57)
+		{
+			c += (s[i] - 48);
+			c *= 10;
+			i++;
+		}
+		c /= 10;
+		return (c * a);
+	}
+	else
+		return (0);
+
+}
