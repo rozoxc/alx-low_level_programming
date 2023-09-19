5d8a859d0302417fcfd66f5908f2b5b6a9cb2c11
a@fma.com <you@email.com>
2023-09-19 01:25:19 -0700

    ld

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,14 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	while (i < n - 1)
+	while (i <= n - 1)
 	{
 		printf("%d", a[i]);
-		printf(",");
-		printf(" ");
+		if (i < n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
 		i++;
 	}
 	printf("\n");
