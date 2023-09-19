bc9717b98b1d5ea83984c5294710dcdab2278470
a@fma.com <you@email.com>
2023-09-18 17:43:19 -0700

    ld

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - function return the size of strin
+ *  @s : stringi
+ *
+ *  Return: return integers
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * puts_half - function print half string
+ * @str: the string
+ */
+
+void puts_half(char *str)
+{
+	int i, x;
+
+	i = 0;
+	x = (_strlen(str) / 2) - 1;
+	while (str[i] != '\0')
+	{
+		if (i > x)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
