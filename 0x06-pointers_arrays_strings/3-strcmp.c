0fcf7b0402ba609691bfe873f77d6b292c8ec770
a@fma.com <you@email.com>
2023-09-21 09:01:36 -0700

    ld

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _strcmp - function compare two string
+ * @s1: first string
+ * @s2: seconde string
+ *
+ * Return: return the soustraction
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
