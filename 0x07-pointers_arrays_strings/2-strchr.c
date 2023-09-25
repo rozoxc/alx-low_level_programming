4243cf7aca4456f384f867152ee3e2b1740c733b
a@fma.com <you@email.com>
2023-09-25 12:54:37 -0700

    ld

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - function that locates a character in a string
+ * @s: the string whow got the character
+ * @c: the character
+ * Return: return the last arrary
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
