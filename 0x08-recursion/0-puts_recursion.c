b7a22e922cc75048d2830c2748ac7b793b3eca9c
a@fma.com <you@email.com>
2023-09-28 09:37:06 -0700

    ls

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
+/**
+ * _puts_recursion -  writes the string s
+ * @s: the string
+ */
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
