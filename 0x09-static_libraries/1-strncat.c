1dfec3384a11b3ebb751e55582c9bfec91ba9153
a@fma.com <you@email.com>
2023-10-02 06:52:58 -0700

    ls

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area.
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: number of bytes to copy.
+ * _strncat - same behavior of strncat() (concatenates two strings.).
+ * @dest: destination.
+ * @src: source.
+ * @n: number of character to copy from src.
  * Return: @dest.
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int i;
+	int i;
+	int j;
 
 	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
+	j = 0;
+	while (dest[i])
 		i++;
+	while ((j < n) && src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
