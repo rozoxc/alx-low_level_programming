d99a9e3f4a5e148de06ba914f6e8a48cda28aed4
a@fma.com <you@email.com>
2023-10-02 14:07:16 -0700

    ls

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: The number of command line arguments
+ *@argv: An array containing the program command line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	i = 1;
+	j = 0;
+	while (i < argc)
+	{
+		j += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
