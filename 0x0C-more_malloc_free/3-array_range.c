d718b891c9ffa0918ced5e0b864acda2a820761b
a@fma.com <you@email.com>
2023-10-07 17:15:59 -0700

    ls

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	i = 0;
-	ptr = malloc(sizeof(int) * (max + min + 1));
+	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 		return (NULL);
 	while (min <= max)
