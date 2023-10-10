e03f3981b9b801622b5060027491d232b211d873
a@fma.com <you@email.com>
2023-10-10 11:24:01 -0700

    kls

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - simple structure named dog.
+ * @name: variable of type char *.
+ * @age: variable of type float.
+ * @owner: variable of type char *.
+ */
+
 struct dog
 {
 	char *name;
