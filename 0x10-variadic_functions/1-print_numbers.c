38951620d1ac067d15f89f1313f1059b86f9c45c
a@fma.com <you@email.com>
2023-10-12 14:18:35 -0700

    ls

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(a, int));
 		if (separator != NULL && i != n - 1)
-			printf("%s ", separator);
+		{
+			printf("%s", separator);
+		}
 		i++;
 	}
 	printf("\n");
