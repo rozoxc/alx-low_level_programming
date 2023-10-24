c2bfdc43b8c810edcdae66f7ff5fb562697c7a0d
abdrrahmane abdoullah <abdel.brave@gmail.com>
2023-10-24 16:03:35 -0700

    ls

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,9 +8,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t;
-
-	*new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *node;
 
 	if (!head || !new_node)
