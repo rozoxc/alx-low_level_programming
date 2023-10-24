af1391e436557b9a6ca157dc8c36b1dae8e09f80
abdrrahmane abdoullah <abdel.brave@gmail.com>
2023-10-24 13:55:16 -0700

    l

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,10 +11,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t  *new_node;
 
 	new_node  = malloc(sizeof(struct listint_s));
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
+	new_node->next = NULL;
 	new_node->n = n;
-	new_node->next = *head;
+	if (*head)
+		new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
