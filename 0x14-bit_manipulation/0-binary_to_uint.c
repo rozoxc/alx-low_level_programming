fedc0c720cd90c0eec05b8553628143c12cfcbda
abdrrahmane abdoullah <abdel.brave@gmail.com>
2023-10-26 18:18:29 -0700

    ls

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,6 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
 
-	j = lenght(b) - 1;
 	result = 0;
 	if (b == NULL)
 		return (0);
@@ -21,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 		}
 		else
 			return (0);
-		*b++;
+		b++;
 	}
 	return (result);
 }
