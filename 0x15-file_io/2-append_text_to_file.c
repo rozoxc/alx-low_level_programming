b18a64e09f635d7d52c89909c26bc74f4477357a
abdrrahmane abdoullah <abdel.brave@gmail.com>
2023-10-30 13:54:15 -0700

    ls

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	file = fopen(filename, "a");
 	if (file == NULL)
