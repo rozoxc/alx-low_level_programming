07ba18c53e097fe0e391e77d0fa8a42a6e4b397f
abdrrahmane abdoullah <abdel.brave@gmail.com>
2023-10-30 16:28:27 -0700

    ls

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	file = fopen(filename, "w");
 	if (file == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (!text_content)
 	{
 		fclose(file);
 		return (1);
