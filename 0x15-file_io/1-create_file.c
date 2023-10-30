0fd35b29d95e33b5371ec1fde8de7f97f72a86e4
abdrrahmane abdoullah <abdel.brave@gmail.com>
2023-10-30 09:04:49 -0700

    ls

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file -  function that creates a file.
+ * @filename:  is the name of the file to create
+ * @text_content: the content of this file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	FILE *file;
+
+	if (filename == NULL)
+		return (-1);
+	file = fopen(filename, "w");
+	if (file == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	fprintf(file, "%s", text_content);
+	return (1);
+}
