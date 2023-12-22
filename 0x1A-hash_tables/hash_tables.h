8ee31f73d9d6cea8c76283ab53344a59206c8c44
he <abdel.brave@gmail.com>
2023-12-22 15:49:09 -0800

    ls

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,6 +9,8 @@
  * @next: A pointer to the next node of the List
  */
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 typedef struct hash_node_s
 {
      char *key;
@@ -32,4 +34,5 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 #endif
