f3644f64b3cfb7744755970d8d36fd0c2a4a7f7f
he <abdel.brave@gmail.com>
2023-12-22 16:11:14 -0800

    ls

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table to print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			flag = 1;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
